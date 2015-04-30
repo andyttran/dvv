61d6d8bd04690216b5dc737a322ef06a151df533
Nathan Bailey <nbailey0001@gmail.com>
2015-04-30 15:54:04 -0700

    slaveProcess performs GET request for data from server on click

---
+++ scripts/slaveProcess.js
@@ -0,0 +1,38 @@
+
+var xhrSuccess = function() {
+  if (this.readyState === 4) {
+    if (this.status === 200) {
+      console.log("RESPONSE TEXT: ", this.responseText);
+      this.callback(JSON.parse(this.responseText));
+    } else {
+      console.error(this.statusText);
+    }
+  }
+};
+
+var xhrError = function() {
+  console.error(this.statusText);
+};
+
+var loadChunk = function(url, callback, timeout) {
+  var oReq = new XMLHttpRequest();
+  oReq.callback = callback;
+  oReq.ontimeout = function () {
+    console.error("The request for " + url + " timed out.");
+  };
+  oReq.onload = xhrSuccess;
+  oReq.onerror = xhrError;
+  oReq.open("get", url, true);
+  oReq.timeout = timeout;
+  oReq.send(null);
+};
+
+var outputMsg = function(data) {
+  console.log("JSON inside callback ", data)
+}
+
+var getData = function(){
+  loadChunk("/getData", outputMsg, 5000);
+};
+
+
